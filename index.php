1f92264518728b51b798c3fbe2f1e1f583131484
edsay12 <edvanadearaujo2@hotmail.com>
2020-11-10 14:32:59 -0300

    2

--- index.php
+++ index.php
@@ -1,16 +1,42 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto final</title>
+    <!-- bootstrao -->
+<link rel="stylesheet" href="app/assets/css/bootstrap.css">
+<!-- outros -->
+<link rel="stylesheet" href="app/assets/icons/icon.css">
+<!-- meu css -->
+<link rel="stylesheet" href="app/assets/css/meucss.css">
+
+
+
 </head>
 <body>
     
 
+<!-- header -->
+<!-- fim header -->
+
+<!-- Paginas do meu site -->
+<!-- Fim paginas do meu site -->
 
 
+<span class="icon-error  text-danger u" ></span>
+
+<!-- Rodape -->
+<!-- Fim Rodape -->
 
 
 </body>
+
+<script src="app/assets/js/jquery.js"></script>
+<script src="app/assets/js/bootstrap.js"></script>
+<script src="app/assets/js/meujs.js"></script>
+
+
+
 </html>
+
